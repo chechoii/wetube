752308ba42129bdc5b6cf173ee1aca59c20e204f
CHECHO2 <mijjn@naver.com>
2025-02-15 20:51:25 +0900

    #3.3

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("I still love you.");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+  console.log(`🆗 Server listening on port http://localhost:${PORT} ❤️`);
+
+app.listen(PORT, handleListening);
